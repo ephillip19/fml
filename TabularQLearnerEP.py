f15f02650b9e36f374dda15bae40145dccc2cb2c
Evan Phillips <evan@dhcp-196-35.student-wlan.bowdoin.edu>
2022-04-22 14:22:31 -0400

    set-up for q-trading lab

---
+++ TabularQLearnerEP.py
@@ -0,0 +1,99 @@
+# TabularQLearnerEP.py
+# Evan Phillips and Sumer Vaidya
+# CSCI 3465
+
+import numpy as np
+import pandas as pd
+import random
+
+
+class TabularQLearner:
+    def __init__(
+        self,
+        states=100,
+        actions=4,
+        alpha=0.2,
+        gamma=0.9,
+        epsilon=0.98,
+        epsilon_decay=0.999,
+        dyna=0,
+    ):
+
+        self.states = states
+        self.actions = actions
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.dyna = dyna
+        self.q_table = np.random.uniform(low=-0.25, high=0.25, size=(states, actions))
+        self.prev_state = None
+        self.prev_action = None
+        self.experience_tuples = []
+
+    def train(self, s, r):
+        # Receive new state s and new reward r.  Update Q-table and return selected action.
+        # used to move the robot around the world
+
+        best_action = self.get_action(s)
+
+        old_q = (1 - self.alpha) * self.q_table[self.prev_state][self.prev_action]
+        new_q = self.alpha * (r + (self.gamma * self.q_table[s][best_action]))
+        self.q_table[self.prev_state][self.prev_action] = old_q + new_q
+
+        experience_tuple = (self.prev_state, self.prev_action, s, r)
+        self.experience_tuples.append(experience_tuple)
+
+        #### DYNA-Q ######
+        for i in range(self.dyna):
+            # pick random indice
+            rand_indice = random.randint(0, len(self.experience_tuples) - 1)
+            rand_exp = self.experience_tuples[rand_indice]
+            last_state, last_action, new_state, reward = (
+                rand_exp[0],
+                rand_exp[1],
+                rand_exp[2],
+                rand_exp[3],
+            )
+
+            best_action_dq = self.get_action(new_state)
+
+            old_q_dq = (1 - self.alpha) * self.q_table[last_state][last_action]
+            new_q_dq = self.alpha * (
+                reward + (self.gamma * self.q_table[new_state][best_action_dq])
+            )
+            self.q_table[last_state][last_action] = old_q_dq + new_q_dq
+
+        # add element of randomness (epsilon-greedy)
+        if np.random.random() < self.epsilon:
+            best_action = random.randint(0, self.actions - 1)
+            self.epsilon *= self.epsilon_decay
+
+        self.prev_state = s
+        self.prev_action = best_action
+
+        return best_action
+
+    def test(self, s):
+        # Receive new state s.  Do NOT update Q-table, but still return selected action.
+
+        best_action = self.get_action(s)
+        if self.prev_state is None and self.prev_action is None:
+            self.prev_state = s
+            self.prev_action = best_action
+
+        self.prev_state = s
+
+        return best_action
+
+    def get_action(self, new_state):
+        # find best action
+        max_q = -100000
+        best_action = None
+        actions = list(range(self.actions))
+        for act in actions:
+            if self.q_table[new_state][act] > max_q:
+                max_q = self.q_table[new_state][act]
+                best_action = act
+
+        return best_action
---
+++ stock_env.py
@@ -0,0 +1,48 @@
+# stock_env.py
+# Evan Phillips and Sumer Vaidya
+# CSCI 3465
+
+
+class StockEnvironment:
+    def __init__(self, fixed=None, floating=None, starting_cash=None, share_limit=None):
+        self.shares = share_limit
+        self.fixed_cost = fixed
+        self.floating_cost = floating
+        self.starting_cash = starting_cash
+
+    def prepare_world(self, start_date, end_date, symbol, data_folder):
+        """
+        Read the relevant price data and calculate some indicators.
+        Return a DataFrame containing everything you need.
+        """
+        pass
+
+    def calc_state(self, df, day, holdings):
+        """Quantizes the state to a single number."""
+        pass
+
+    def train_learner(
+        self, start=None, end=None, symbol=None, trips=0, dyna=0, eps=0.0, eps_decay=0.0
+    ):
+        """
+        Construct a Q-Learning trader and train it through many iterations of a stock
+        world.  Store the trained learner in an instance variable for testing.
+
+        Print a summary result of what happened at the end of each trip.
+        Feel free to include portfolio stats or other information, but AT LEAST:
+
+        Trip 499 net result: $13600.00
+        """
+        pass
+
+    def test_learner(self, start=None, end=None, symbol=None):
+        """
+        Evaluate a trained Q-Learner on a particular stock trading task.
+        Print a summary result of what happened during the test.
+
+        Feel free to include portfolio stats or other information, but AT LEAST:
+
+        Test trip, net result: $31710.00
+        Benchmark result: $6690.0000
+        """
+        pass
